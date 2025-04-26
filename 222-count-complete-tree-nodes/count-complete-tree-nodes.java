f8fe36ce71e153a9586549120e210688b98646a6
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-04-27 01:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (5.53%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -15,25 +15,23 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        // if(root == null) return 0;
-        // int lh = 0, rh = 0;
-        // TreeNode curr = root;
-        // while(curr.left != null){
-        //     lh++;
-        //     curr = curr.left;
-        // }
-        // curr = root;
-        // while(curr.right != null){
-        //     rh++;
-        //     curr = curr.right;
-        // }
-        // if(lh == rh){
-        //     return (int)Math.pow(2, lh+1) -1;
-        // }
-        // return 1+countNodes(root.left)+countNodes(root.right);
-
         if(root==null) return 0;
-        else return 1+countNodes(root.left)+countNodes(root.right);
+        int lh=0, rh=0;
+        TreeNode curr = root;
+        while(curr!=null){
+            lh++;
+            curr = curr.left;
+        }
+        curr=root;
+        while(curr!=null){
+            rh++;
+            curr = curr.right;
+        }
+        if(lh==rh) return (int)Math.pow(2, lh)-1;
+        return 1+countNodes(root.left)+countNodes(root.right);
+
+        // if(root==null) return 0;
+        // else return 1+countNodes(root.left)+countNodes(root.right);
 
 
         // int size = 1;
