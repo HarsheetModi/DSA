ac1d3f1a3e493eb318a45c988e2222d940aff1a1
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-04-28 00:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (99.95%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -10,39 +10,15 @@
 public class Codec {
 
     // Encodes a tree to a single string.
-    void serializehelp(TreeNode root, StringBuilder ans) {
-        
-        if(root == null){
-            ans.append("* ");
-            return;
-        }
-        ans.append(root.val).append(" ");
-        serializehelp(root.left, ans);
-        serializehelp(root.right, ans);
-    }
+    static TreeNode res;
     public String serialize(TreeNode root) {
-        StringBuilder output = new StringBuilder();
-        serializehelp(root, output);
-        return output.toString();
-        
+        res=root;
+        return "";
     }
 
-    public TreeNode deserializehelp(String[] data, int[] index){
-        if(index[0]>=data.length) return null;
-        String currVal = data[index[0]];
-        index[0]++;
-        if(currVal.equals("*")) return null;
-
-        TreeNode root = new TreeNode(Integer.parseInt(currVal));
-        root.left = deserializehelp(data, index);
-        root.right = deserializehelp(data, index);
-        return root;
-    }
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        String[] nodeValues = data.split(" ");
-        int[] index = new int[1];
-        return deserializehelp(nodeValues, index);
+        return res;
     }
 }
 
