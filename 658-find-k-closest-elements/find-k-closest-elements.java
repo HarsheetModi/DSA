5616a3b7408101b58e1fbd1199c4329be26f59b9
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-05-06 23:36:42 +0530

    Time: 29 ms (22.14%) | Memory: 46.4 MB (13.71%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,22 +1,52 @@
 class Solution {
 
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int start = 0;
-        int end = arr.length-1;
-        while(end - start >= k){
-            if(Math.abs(arr[end]-x) < Math.abs(arr[start]-x)){
-                start++;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int integer: arr){
+            if(k>0){
+                minHeap.offer(integer);
+                k--;
             }
-            else{
-                end--;
+            else if(Math.abs(minHeap.peek()-x)>Math.abs(integer-x)){
+                minHeap.poll();
+                minHeap.offer(integer);
             }
         }
         List<Integer> result = new ArrayList<>();
-        while(end-start >=0){
-            result.add(arr[start]);
-            start++;
+        while(!minHeap.isEmpty()){
+            result.add(minHeap.poll());
         }
         return result;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // int start = 0;
+        // int end = arr.length-1;
+        // while(end - start >= k){
+        //     if(Math.abs(arr[end]-x) < Math.abs(arr[start]-x)){
+        //         start++;
+        //     }
+        //     else{
+        //         end--;
+        //     }
+        // }
+        // List<Integer> result = new ArrayList<>();
+        // while(end-start >=0){
+        //     result.add(arr[start]);
+        //     start++;
+        // }
+        // return result;
     }
 
 
