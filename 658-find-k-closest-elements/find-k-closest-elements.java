1c60537d1d98fb0077c4db84abf97a96db186fbb
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-05-06 23:28:00 +0530

    Time: 4 ms (97.40%) | Memory: 46.2 MB (26.94%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,23 +1,58 @@
 class Solution {
+
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> result = new ArrayList<>();
-        boolean[] boolArray = new boolean[arr.length];
-        
-        for(int i =0;i<k;i++){
-            int min_diff = Integer.MAX_VALUE;
-            int min_diff_idx=-1;
-            for(int j=0;j<arr.length;j++){
-                if(boolArray[j]==false && Math.abs(arr[j]-x)<min_diff){
-                    min_diff = Math.abs(arr[j]-x);
-                    min_diff_idx = j;
-                }
+        int start = 0;
+        int end = arr.length-1;
+        while(end - start >= k){
+            if(Math.abs(arr[end]-x) < Math.abs(arr[start]-x)){
+                start++;
             }
-            if(min_diff_idx != -1){
-                result.add(arr[min_diff_idx]);
-                boolArray[min_diff_idx] = true;
+            else{
+                end--;
             }
         }
-        Collections.sort(result);
+        List<Integer> result = new ArrayList<>();
+        while(end-start >=0){
+            result.add(arr[start]);
+            start++;
+        }
         return result;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     List<Integer> result = new ArrayList<>();
+    //     boolean[] boolArray = new boolean[arr.length];
+        
+    //     for(int i =0;i<k;i++){
+    //         int min_diff = Integer.MAX_VALUE;
+    //         int min_diff_idx=-1;
+    //         for(int j=0;j<arr.length;j++){
+    //             if(boolArray[j]==false && Math.abs(arr[j]-x)<min_diff){
+    //                 min_diff = Math.abs(arr[j]-x);
+    //                 min_diff_idx = j;
+    //             }
+    //         }
+    //         if(min_diff_idx != -1){
+    //             result.add(arr[min_diff_idx]);
+    //             boolArray[min_diff_idx] = true;
+    //         }
+    //     }
+    //     Collections.sort(result);
+    //     return result;
+    // }
 }
