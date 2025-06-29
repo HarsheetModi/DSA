46672c25ffc87929a76ec3fc75f015beb1361c5e
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-06-29 15:27:42 +0530

    Time: 3 ms (84.50%) | Memory: 49.5 MB (61.42%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,22 +1,23 @@
 class Solution {
     public int numIslands(char[][] grid) {
-    int count=0;
-    for(int i=0;i<grid.length;i++)
-        for(int j=0;j<grid[0].length;j++){
-            if(grid[i][j]=='1'){
-                dfsFill(grid,i,j);
-                count++;
+        int count =0;
+        for(int i =0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid, i, j);
+                    count++;
+                }
             }
         }
-    return count;
-}
-private void dfsFill(char[][] grid,int i, int j){
-    if(i>=0 && j>=0 && i<grid.length && j<grid[0].length&&grid[i][j]=='1'){
-        grid[i][j]='0';
-        dfsFill(grid, i + 1, j);
-        dfsFill(grid, i - 1, j);
-        dfsFill(grid, i, j + 1);
-        dfsFill(grid, i, j - 1);
+        return count;
     }
-}
+    public void dfs(char[][] grid, int r, int c){
+        if(r>=0 && c>=0 && r<grid.length && c<grid[0].length && grid[r][c]=='1'){
+            grid[r][c] ='0';
+            dfs(grid, r+1, c);
+            dfs(grid, r-1, c);
+            dfs(grid, r, c+1);
+            dfs(grid, r, c-1);
+        }
+    }
 }
