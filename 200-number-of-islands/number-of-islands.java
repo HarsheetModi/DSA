a4fa24f4c2236062ae05a535753ac6b349317bcf
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-06-29 15:22:33 +0530

    Time: 3 ms (84.50%) | Memory: 49.3 MB (85.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,39 +1,22 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        boolean[][] visited = new boolean[rows][cols];
-        int count =0;
-        for(int i =0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]=='1' && !visited[i][j]){
-                    bfs(grid, visited, i, j);
-                    count++;
-                }
+    int count=0;
+    for(int i=0;i<grid.length;i++)
+        for(int j=0;j<grid[0].length;j++){
+            if(grid[i][j]=='1'){
+                dfsFill(grid,i,j);
+                count++;
             }
         }
-        return count;
+    return count;
+}
+private void dfsFill(char[][] grid,int i, int j){
+    if(i>=0 && j>=0 && i<grid.length && j<grid[0].length&&grid[i][j]=='1'){
+        grid[i][j]='0';
+        dfsFill(grid, i + 1, j);
+        dfsFill(grid, i - 1, j);
+        dfsFill(grid, i, j + 1);
+        dfsFill(grid, i, j - 1);
     }
-    public void bfs(char[][] grid, boolean[][] visited, int startRow, int startCol){
-        int rows = grid.length;
-        int cols = grid[0].length;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{startRow, startCol});
-        visited[startRow][startCol] = true;
-
-        int[][] directions = {{1,0}, {-1,0},{0,1}, {0,-1}};
-        while(!queue.isEmpty()){
-            int[] curr = queue.poll();
-            int row = curr[0], col = curr[1];
-            for(int[] dir : directions){
-                int r = row + dir[0];
-                int c = col + dir[1];
-                if(r<0 || r >= rows || c<0 || c>=cols || grid[r][c]=='0' || visited[r][c]){
-                    continue;
-                }
-                queue.offer(new int[]{r,c});
-                visited[r][c] = true;
-            }
-        }
-    }
+}
 }
