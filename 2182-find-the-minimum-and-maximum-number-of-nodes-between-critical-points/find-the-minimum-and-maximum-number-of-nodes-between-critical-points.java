580c64555721be1de1294a1c1090ab58802bac3c
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-07-10 22:34:47 +0530

    Time: 5 ms (54.00%) | Memory: 61 MB (43.95%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -9,38 +9,63 @@
  * }
  */
 class Solution {
-    public int[] nodesBetweenCriticalPoints(ListNode head) {
-        int[] result = {-1,-1};
+    // public int[] nodesBetweenCriticalPoints(ListNode head) {
+    //     int[] result = {-1,-1};
 
-        if(head == null || head.next == null || head.next.next == null){
-            return result;
-        }
-        List<Integer> vals = new ArrayList<>();
-        int count = 1;
-        ListNode prev = head;
-        ListNode curr = head.next;
-        ListNode next = curr.next;
-        while(next != null){
-            if(curr.val > prev.val && curr.val > next.val){
-                vals.add(count);
-            }
-            else if(curr.val < prev.val && curr.val < next.val){
-                vals.add(count);
-            }
-            prev = curr;
-            curr = next;
-            next = next.next;
-            count++;
-        }
-        if (vals.size() < 2) return result;
+    //     if(head == null || head.next == null || head.next.next == null){
+    //         return result;
+    //     }
+    //     List<Integer> vals = new ArrayList<>();
+    //     int count = 1;
+    //     ListNode prev = head;
+    //     ListNode curr = head.next;
+    //     ListNode next = curr.next;
+    //     while(next != null){
+    //         if(curr.val > prev.val && curr.val > next.val){
+    //             vals.add(count);
+    //         }
+    //         else if(curr.val < prev.val && curr.val < next.val){
+    //             vals.add(count);
+    //         }
+    //         prev = curr;
+    //         curr = next;
+    //         next = next.next;
+    //         count++;
+    //     }
+    //     if (vals.size() < 2) return result;
 
-        int min = Integer.MAX_VALUE;
-        for(int i =1; i < vals.size(); i++){
-            min = Math.min(min, vals.get(i)-vals.get(i-1));
+    //     int min = Integer.MAX_VALUE;
+    //     for(int i =1; i < vals.size(); i++){
+    //         min = Math.min(min, vals.get(i)-vals.get(i-1));
+    //     }
+    //     int max = vals.get(vals.size()-1) - vals.get(0);
+    //     result[1] = max;
+    //     result[0] = min;
+    //     return result;
+    // }
+     public int[] nodesBetweenCriticalPoints(ListNode head) {
+        ListNode pre = head;
+        ListNode cur = head.next;
+        int[] ans = {-1, -1};
+        int prePosition = -1, curPosition = -1, firstPosition = -1, position = 0;
+        while (cur.next != null) {
+            if ((cur.val < pre.val && cur.val < cur.next.val) || (cur.val > pre.val && cur.val > cur.next.val)) {
+                // local
+                prePosition = curPosition;
+                curPosition = position;
+                if (firstPosition == -1) {
+                    firstPosition = position;
+                }
+                if (prePosition != -1) {
+                    if (ans[0] == -1) ans[0] = curPosition - prePosition;
+                    else ans[0] = Math.min(ans[0], curPosition - prePosition);
+                    ans[1] = position - firstPosition;
+                }
+            }
+            position++;
+            pre = cur;
+            cur = cur.next;
         }
-        int max = vals.get(vals.size()-1) - vals.get(0);
-        result[1] = max;
-        result[0] = min;
-        return result;
+        return ans;
     }
 }
