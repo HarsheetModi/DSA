7dab93c8a09492dcd75056df5f09e69157df3d59
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-09-07 23:23:03 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (47.30%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,29 +1,89 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-
         int m = matrix.length, n = matrix[0].length;
-        int rowsArr[] = new int[m];
-        int colsArr[] = new int[n];
-
-        Arrays.fill(rowsArr, 1);
-        Arrays.fill(colsArr, 1);
-
+        boolean colsFlag=false, rowsFlag=false;
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+            if(matrix[i][0] == 0){
+                rowsFlag = true;
+            }
+        }
+        for(int j=0; j<n; j++){
+            if(matrix[0][j] == 0){
+                colsFlag = true;
+            }
+        }
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
                 if(matrix[i][j]==0){
-                    rowsArr[i] = 0;
-                    colsArr[j] = 0;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(rowsArr[i]==0 || colsArr[j]==0){
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
                     matrix[i][j] = 0;
                 }
             }
         }
+        if(rowsFlag){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if(colsFlag){
+            for(int i=0; i<n; i++){
+                matrix[0][i] = 0;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int m = matrix.length, n = matrix[0].length;
+        // int rowsArr[] = new int[m];
+        // int colsArr[] = new int[n];
+
+        // Arrays.fill(rowsArr, 1);
+        // Arrays.fill(colsArr, 1);
+
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(matrix[i][j]==0){
+        //             rowsArr[i] = 0;
+        //             colsArr[j] = 0;
+        //         }
+        //     }
+        // }
+
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(rowsArr[i]==0 || colsArr[j]==0){
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
 
 
 
