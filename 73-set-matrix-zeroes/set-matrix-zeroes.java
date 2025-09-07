e86559b921b466cd4328a82b469d43254c08118c
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-09-07 23:07:43 +0530

    Time: 1 ms (71.36%) | Memory: 45.9 MB (16.33%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,30 +1,75 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        int copyMatrix[][] = new int[m][n];
-        for(int i =0;i<m;i++){
-            for(int j=0;j<n;j++){
-                copyMatrix[i][j] = matrix[i][j];
-            }
-        }
 
-        for(int i =0; i<m; i++){
+        int m = matrix.length, n = matrix[0].length;
+        int rowsArr[] = new int[m];
+        int colsArr[] = new int[n];
+
+        Arrays.fill(rowsArr, 1);
+        Arrays.fill(colsArr, 1);
+
+        for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(matrix[i][j] == 0){
-                    for(int a =0; a<n; a++){
-                        copyMatrix[i][a] = 0;
-                    }
-                    for(int b=0; b<m; b++){
-                        copyMatrix[b][j] = 0;
-                    }
+                if(matrix[i][j]==0){
+                    rowsArr[i] = 0;
+                    colsArr[j] = 0;
                 }
             }
         }
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                matrix[i][j] = copyMatrix[i][j];
+                if(rowsArr[i]==0 || colsArr[j]==0){
+                    matrix[i][j] = 0;
+                }
             }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int m = matrix.length, n = matrix[0].length;
+        // int copyMatrix[][] = new int[m][n];
+        // for(int i =0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         copyMatrix[i][j] = matrix[i][j];
+        //     }
+        // }
+
+        // for(int i =0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(matrix[i][j] == 0){
+        //             for(int a =0; a<n; a++){
+        //                 copyMatrix[i][a] = 0;
+        //             }
+        //             for(int b=0; b<m; b++){
+        //                 copyMatrix[b][j] = 0;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         matrix[i][j] = copyMatrix[i][j];
+        //     }
+        // }
     }
 }
