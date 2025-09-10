7ef1983d6bc7a0ead7ddd38a1c70a20386dddaf1
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-09-10 12:00:33 +0530

    Time: 1 ms (99.49%) | Memory: 57.2 MB (42.91%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,14 +1,27 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max_so_far = nums[0];
-        int max_ending_here = nums[0];
-
-        for(int i=1; i<nums.length; i++){
-            max_ending_here = Math.max(nums[i], nums[i]+ max_ending_here);
-            max_so_far = Math.max(max_so_far, max_ending_here);
+        int sum = 0, max = nums[0];
+        for(int i =0; i<nums.length; i++){
+            sum = sum + nums[i];
+            if(sum > max){
+                max = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
         }
+        return max;
 
-    return max_so_far;
+
+    //     int max_so_far = nums[0];
+    //     int max_ending_here = nums[0];
+
+    //     for(int i=1; i<nums.length; i++){
+    //         max_ending_here = Math.max(nums[i], nums[i]+ max_ending_here);
+    //         max_so_far = Math.max(max_so_far, max_ending_here);
+    //     }
+
+    // return max_so_far;
 
 
 
