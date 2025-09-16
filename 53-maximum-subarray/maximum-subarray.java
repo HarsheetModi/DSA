aa1735d5eecefe5afdfab112d85f4677d684afb4
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-09-16 20:50:19 +0530

    Time: 1 ms (99.49%) | Memory: 57.4 MB (20.73%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,9 +3,7 @@ class Solution {
         int sum = 0, max = nums[0];
         for(int i =0; i<nums.length; i++){
             sum = sum + nums[i];
-            if(sum > max){
-                max = sum;
-            }
+            max = Math.max(sum, max);
             if(sum < 0){
                 sum = 0;
             }
