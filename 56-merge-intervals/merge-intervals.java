8e3d134bca23d8ae72723a55b56d73ac86f874bb
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-09-20 12:15:33 +0530

    Time: 132 ms (6.01%) | Memory: 47.3 MB (6.24%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -23,7 +23,7 @@ class Solution {
         for(int i=1; i<intervals.length; i++){
             if(intervals[i][0] > result.get(result.size()-1).get(1)){
                 result.add(new ArrayList<>(Arrays.asList(intervals[i][0], intervals[i][1])));
-            }
+            } 
             else{
                 result.get(result.size()-1).set(1, Math.max(intervals[i][1], result.get(result.size()-1).get(1)));
             }
