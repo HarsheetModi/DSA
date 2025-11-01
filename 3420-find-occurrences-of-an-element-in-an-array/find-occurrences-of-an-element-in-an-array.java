3dfff66346746892bc868e9efec74319aaa8b32b
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-11-02 01:21:41 +0530

    Time: 5 ms (90.27%) | Memory: 163.6 MB (6.19%) - LeetSync

--- 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
@@ -1,35 +1,53 @@
 class Solution {
     public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        // HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
 
-        for(int i=0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-                ArrayList<Integer> list = map.get(nums[i]);
-                list.set(0, list.get(0) + 1);
-                list.add(i);
-            }
-            else{
-                ArrayList<Integer> values = new ArrayList<>();
-                values.add(1);
-                values.add(i);
-                map.put(nums[i], values);
-            }
-        }
+        // for(int i=0; i<nums.length; i++){
+        //     if(map.containsKey(nums[i])){
+        //         ArrayList<Integer> list = map.get(nums[i]);
+        //         list.set(0, list.get(0) + 1);
+        //         list.add(i);
+        //     }
+        //     else{
+        //         ArrayList<Integer> values = new ArrayList<>();
+        //         values.add(1);
+        //         values.add(i);
+        //         map.put(nums[i], values);
+        //     }
+        // }
        
 
-        for(int i =0; i<queries.length; i++){
-            if(!map.containsKey(x)){
-                queries[i] = -1;
+        // for(int i =0; i<queries.length; i++){
+        //     if(!map.containsKey(x)){
+        //         queries[i] = -1;
+        //     }
+        //     else{
+        //         ArrayList<Integer> list = map.get(x);
+        //         int count = list.get(0);
+        //         if(count >= queries[i]){
+        //             queries[i] = list.get(queries[i]);
+        //         }
+        //         else{
+        //             queries[i] = -1;
+        //         }
+        //     }
+        // }
+        // return queries;
+
+
+        List<Integer> value = new ArrayList<>();
+        
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==x){
+                value.add(i);
+            }
+        }
+        for(int i=0; i<queries.length; i++){
+            if(queries[i]>value.size()){
+                queries[i]=-1;
             }
             else{
-                ArrayList<Integer> list = map.get(x);
-                int count = list.get(0);
-                if(count >= queries[i]){
-                    queries[i] = list.get(queries[i]);
-                }
-                else{
-                    queries[i] = -1;
-                }
+                queries[i] = value.get(queries[i]-1);
             }
         }
         return queries;
