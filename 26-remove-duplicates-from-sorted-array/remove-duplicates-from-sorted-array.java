7c61cf5ebd805082d3083296a2e83792f02d1a8d
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-11-21 10:51:31 +0530

    Time: 1 ms (60.96%) | Memory: 46.7 MB (10.40%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,25 +1,31 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         //brute force
-        Set<Integer> set = new LinkedHashSet<>();
-        for(int i=0; i<nums.length; i++){
-            set.add(nums[i]);
-        }
-
-        int index = 0;
-        for(Integer s: set){
-            nums[index++] = s;
-        }
-
-        return set.size();
-
-        // int position = 0;
-        // for(int i =1; i<nums.length; i++){
-        //     if(nums[position] != nums[i]){
-        //         position++;
-        //         nums[position] = nums[i];
-        //     }
+        //0(NlogN) insetion for set
+        //0(N) for retrieval from set
+        //total time: 0(NlogN + N)
+        // Set<Integer> set = new LinkedHashSet<>();
+        // for(int i=0; i<nums.length; i++){
+        //     set.add(nums[i]);
         // }
-        // return position+1;
+
+        // int index = 0;
+        // for(Integer s: set){
+        //     nums[index++] = s;
+        // }
+
+        // return set.size();
+
+
+        // optimal solution
+        int position = 0;
+        for(int i =1; i<nums.length; i++){
+            if(nums[position] != nums[i]){
+                
+                nums[position+1] = nums[i];
+                position++;
+            }
+        }
+        return position+1;
     }
 }
