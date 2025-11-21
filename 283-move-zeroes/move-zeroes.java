ab4e0caf95a04b8d1839b181920deaf39d32f369
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-11-21 12:47:56 +0530

    Time: 2 ms (82.84%) | Memory: 47.8 MB (14.66%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,6 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int position=0, curr =0, count=0;
+        int position=0, curr =0;
         while(curr<nums.length){
             if(nums[curr] == 0){
                 curr++;
@@ -9,10 +9,9 @@ class Solution {
                 nums[position] = nums[curr];
                 curr++;
                 position++;
-                count++;
             }
         }
-        for(int i =count; i<nums.length; i++){
+        for(int i =position; i<nums.length; i++){
             nums[i]=0;
         }
 
