74a90491f6669a33a139d8712e451f8a150cb5cf
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-11-22 13:12:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (15.51%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,12 +1,10 @@
 import java.util.Arrays;
 class Solution {
     int[] dp = new int[46];
-    public Solution() {
-        Arrays.fill(dp, -1);
-    } 
+     
     public int climbStairs(int n) {
         if(n<=1) return 1;
-        if(dp[n]!=-1) return dp[n];
+        if(dp[n]!=0) return dp[n];
         return dp[n] = climbStairs(n-1) + climbStairs(n-2);
     }
 }
