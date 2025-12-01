5250e150f3368e0880edc6c08164dbdda70ff8b2
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-12-01 22:36:46 +0530

    Time: 6 ms (46.73%) | Memory: 46.6 MB (23.96%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,75 +1,17 @@
-import java.util.*;
 class Solution {
-    
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> pairs = new HashMap<>();
-        for(int i =0;i<nums.length;i++){
-            int val = target - nums[i];
-            if(pairs.containsKey(val)){
-                return new int[]{i, pairs.get(val) };
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
             }
-            pairs.put(nums[i], i);
+            else{
+                map.put(nums[i], i);
+            }
         }
-        return new int[]{};
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // int[] res = new int[2];
-        // for(int i =0;i<nums.length-1;i++){
-        //     for(int j=i+1;j<nums.length;j++){
-        //         if(nums[i]+nums[j]==target && i!=j){
-        //             res[0]=i;
-        //             res[1]=j;
-        //         }
-        //     }
-        // }
-        // return res;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //     int[] ans = new int[2];
-    //     for(int i =0;i<nums.length;i++){
-    //         for(int j=0;j<nums.length;j++){
-    //             if(i==j){
-    //                 continue;
-    //             }
-    //             else{
-    //                 if(nums[i]+nums[j]==target){
-    //                     ans[0] = i;
-    //                     ans[1] = j;
-    //                     return ans;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return ans;
-    //
-     }
+        return result;
+    }
 }
