19d7abedde8d0dd6157df2071ae13df7cc5ba63c
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-12-02 09:29:09 +0530

    Time: 1 ms (99.64%) | Memory: 77.1 MB (36.28%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,72 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0, max = nums[0];
-        for(int i =0; i<nums.length; i++){
-            sum = sum + nums[i];
-            max = Math.max(sum, max);
-            if(sum < 0){
-                sum = 0;
-            }
+        int maxi = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i: nums){
+            sum += i;
+            maxi = Math.max(maxi, sum);
+            if(sum < 0) sum = 0;
         }
-        return max;
-
-
-    //     int max_so_far = nums[0];
-    //     int max_ending_here = nums[0];
-
-    //     for(int i=1; i<nums.length; i++){
-    //         max_ending_here = Math.max(nums[i], nums[i]+ max_ending_here);
-    //         max_so_far = Math.max(max_so_far, max_ending_here);
-    //     }
-
-    // return max_so_far;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // int res =nums[0];
-        // int maxending = nums[0];
-        // for(int i =1;i<nums.length;i++){
-        //     maxending = Math.max(maxending+nums[i], nums[i]);
-        //     res = Math.max(maxending, res);
-        // }
-        // return res;
-
-
-
-
-
-
-
-
-
-
-        // int res=Integer.MIN_VALUE;
-        // for(int i =0;i<nums.length;i++){
-        //     int cur =0;
-        //     for(int j=i;j<nums.length;j++){
-        //         cur= cur+nums[j];
-        //         if(cur>res){
-        //         res =cur;
-        //     }
-        //     }
-            
-        // }
-        // return res;
+        return maxi;
     }
 }
