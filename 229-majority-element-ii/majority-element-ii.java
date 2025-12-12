5f893931fe0a44fb4ad918e9788a48ee98b08f30
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-12-12 19:54:37 +0530

    Time: 15 ms (26.52%) | Memory: 53 MB (10.29%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+
+        for(int i: nums){
+            map.put(i, map.getOrDefault(i, 0)+ 1);
+        }
+
+        for(int i: map.keySet()){
+            if(map.get(i) > nums.length/3){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
