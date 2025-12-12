87163355de4b479e6a90c135fdb4c6e08e826204
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-12-12 19:44:59 +0530

    Time: 1 ms (99.83%) | Memory: 55.9 MB (7.15%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,32 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int maj =0;
-        for(int i: nums){
-            map.put(i, map.getOrDefault(i, 0)+1);
-        }
-
-        for(int i: map.keySet()){
-            if(map.get(i)>nums.length/2){
-                maj = i;
+        int maj = nums[0];
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            if(maj == nums[i]) count++;
+            else{
+                if(count == 0){
+                    maj = nums[i];
+                    count++;
+                }
+                else{
+                    count--;
+                }
             }
         }
-
         return maj;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int maj =0;
+        // for(int i: nums){
+        //     map.put(i, map.getOrDefault(i, 0)+1);
+        // }
+
+        // for(int i: map.keySet()){
+        //     if(map.get(i)>nums.length/2){
+        //         maj = i;
+        //     }
+        // }
+
+        // return maj;
     }
 }
