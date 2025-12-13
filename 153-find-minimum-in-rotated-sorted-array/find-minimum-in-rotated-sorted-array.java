8d792450cb99528fcf3c0c1f35ec72cc401fceec
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-12-13 10:37:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (30.23%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -3,6 +3,10 @@ class Solution {
         int low = 0, high = nums.length-1, ans = Integer.MAX_VALUE;
         while(low<=high){
             int mid = low + (high-low)/2;
+            if(nums[low] <= nums[high]){
+                ans = Math.min(ans, nums[low]);
+                break;
+            }
             if(nums[low] <= nums[mid]){
                 ans = Math.min(ans, nums[low]);
                 low = mid+1;
@@ -13,5 +17,39 @@ class Solution {
             }
         }
         return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int low = 0, high = nums.length-1, ans = Integer.MAX_VALUE;
+        // while(low<=high){
+        //     int mid = low + (high-low)/2;
+        //     if(nums[low] <= nums[mid]){
+        //         ans = Math.min(ans, nums[low]);
+        //         low = mid+1;
+        //     }
+        //     else{
+        //         ans = Math.min(ans, nums[mid]);
+        //         high = mid-1;
+        //     }
+        // }
+        // return ans;
     }
 }
