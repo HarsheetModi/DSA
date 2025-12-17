73f79b74334518bf8a2765ba247fe91900247a47
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-12-17 12:49:09 +0530

    Time: 4 ms (94.04%) | Memory: 44.1 MB (71.23%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,24 +1,69 @@
 class Solution {
     public String reverseWords(String s) {
-        ArrayList<String> words = new ArrayList<>();
-        StringBuilder word = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        int i = s.length() - 1;
+        while(i >= 0){
 
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) != ' '){
-                word.append(s.charAt(i));
+            while(i >=0 && s.charAt(i) == ' '){
+                i--;
             }
-            else if(word.length() > 0){
-                words.add(word.toString());
-                word.setLength(0);
+
+            if(i < 0) break;
+
+            int end = i;
+
+            while(i >=0 && s.charAt(i) != ' '){
+                i--;
             }
+
+            String word = s.substring(i+1, end+1);
+
+            if(result.length() > 0){
+                result.append(" ");
+            }
+
+            result.append(word);
+
         }
 
-        if(word.length() > 0){
-            words.add(word.toString());
-        }
+        return result.toString();
+        
 
-        Collections.reverse(words);
 
-        return String.join(" ", words);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // ArrayList<String> words = new ArrayList<>();
+        // StringBuilder word = new StringBuilder();
+
+        // for(int i=0; i<s.length(); i++){
+        //     if(s.charAt(i) != ' '){
+        //         word.append(s.charAt(i));
+        //     }
+        //     else if(word.length() > 0){
+        //         words.add(word.toString());
+        //         word.setLength(0);
+        //     }
+        // }
+
+        // if(word.length() > 0){
+        //     words.add(word.toString());
+        // }
+
+        // Collections.reverse(words);
+
+        // return String.join(" ", words);
     }
 }
