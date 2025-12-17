3d007fdebb29bcca194395208675cf0b9245b7e5
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-12-17 18:22:06 +0530

    Time: 1 ms (24.94%) | Memory: 42.5 MB (67.17%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int i;
+        for(i=0; i<s.length(); i++){
+            if(s.charAt(i) == goal.charAt(0)){
+                String s1 = s.substring(0,i);
+                String s2 = s.substring(i);
+
+                if((s2+s1).equals(goal)) return true;
+            }
+        }
+        return false;
+    }
+}
