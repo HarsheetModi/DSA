f3585322bffcfb319a4e7d5207e0f8870e37a4ec
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2025-12-17 17:20:51 +0530

    Time: 20 ms (16.85%) | Memory: 44.2 MB (11.67%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,14 +3,13 @@ class Solution {
         Map<Character, Character> maps = new HashMap<>();
         Map<Character, Character> mapt = new HashMap<>();
         for(int i=0; i<s.length(); i++){
-            if(!maps.containsKey(s.charAt(i)) || !mapt.containsKey(t.charAt(i))){
                 if(!maps.containsKey(s.charAt(i))){
                     maps.put(s.charAt(i), t.charAt(i));
                 }
                 if(!mapt.containsKey(t.charAt(i))){
                     mapt.put(t.charAt(i), s.charAt(i));
                 }
-            }
+            
             if(maps.get(s.charAt(i)) == t.charAt(i) && mapt.get(t.charAt(i)) == s.charAt(i)) continue;
             else return false;
         }
