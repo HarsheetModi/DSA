58ac95c6cb512fc0b67e1f71841c7b4b1e01148e
HarsheetModi <63414698+HarsheetModi@users.noreply.github.com>
2026-01-06 01:47:26 +0530

    Time: 3 ms (8.86%) | Memory: 45.3 MB (5.42%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int myAtoi(String s) {
+        if(s.length() == 0) return 0;
+        int result = 1;
+        int i = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('1', 1);
+        map.put('2', 2);
+        map.put('3', 3);
+        map.put('4', 4);
+        map.put('5', 5);
+        map.put('6', 6);
+        map.put('7', 7);
+        map.put('8', 8);
+        map.put('9', 9);
+        map.put('0', 0);
+        char ch = s.charAt(i);
+        while(ch == ' ' && i<s.length()){
+            i++;
+            if(i<s.length()) ch = s.charAt(i);
+        }
+
+        if(ch == '-'){
+            result = -1;
+            i++;
+        }
+        if(ch == '+') i++;
+        if(i<s.length()){
+            ch = s.charAt(i);
+        }
+        
+        while(ch == '0' && i<s.length()){
+            i++;
+            if(i > s.length()) return 0;
+            if(i < s.length()) ch = s.charAt(i);
+        }
+        if(map.containsKey(ch)){
+            result = result * map.get(ch);
+            i++;
+        }
+        else return 0;
+        while(i < s.length()){
+            ch = s.charAt(i);
+            if(map.containsKey(ch)){
+                if((long)result * 10 + map.get(ch) > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+                else if((long)result * 10 - map.get(ch) < Integer.MIN_VALUE || result - 1 == Integer.MAX_VALUE) return Integer.MIN_VALUE;
+                else if(result < 0) result = result * 10 - map.get(ch);
+                else result = result * 10 + map.get(ch);
+                i++;
+            }
+            else{
+                return result;
+            }
+        }
+        return result;
+    }
+}
